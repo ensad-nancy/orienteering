afd002ee5d285f9ecb9186c87c7ba74f45f0580e
dcfvg <qwsx@dcfvg.com>
2016-03-14 17:23:19 +0100

    js example

---
+++ points/dcfvg/nd-network/assets/js/script.js
@@ -0,0 +1,33 @@
+
+window.onload = function() { init() };
+
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1nPwfhbwXeIv8ksiuX6Q9rj7QBzV6skdr0z_g2gvaL3E/pubhtml';
+
+function init() { Tabletop.init(
+    {
+      key: public_spreadsheet_url,
+      callback: showInfo
+    }
+  )
+}
+
+function showInfo(data){
+
+  console.log(data);
+
+  data.personnes.elements.forEach(function(d){
+
+    var DNSEP = d.DNSEP === "" ? "" : '('+d.DNSEP+')';
+
+    if(d.www != "") $( "#listPersonnes" ).append(
+      '<a class="link" title="'
+      + d.nom +' '+ d.prenom + DNSEP + '" target="_blank" href="'+d.www
+      +'"><img width="20%" src="http://free.pagepeeker.com/v2/thumbs.php?size=x&url='+d.www
+      +'"></a>' );
+  })
+
+  $('.link').mouseover(function() {
+    $('#infoZone').html( $(this).attr('title'))
+  })
+
+}
